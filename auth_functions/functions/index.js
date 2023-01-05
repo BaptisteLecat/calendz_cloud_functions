2ac3dbd57c3a5429c28c9def28c806a84cf32d1d
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2023-01-05 18:40:18 +0100

    Implement Google and Apple provider info

--- auth_functions/functions/index.js
+++ auth_functions/functions/index.js
@@ -4,12 +4,60 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.setupFirestoreUser = functions.region('europe-west1').auth.user().onCreate(async(user) => {
-    await admin.firestore().collection("users").doc(user.uid).set({
-        email: user.email,
-    }).then(() => {
-        console.log(`Successfully created user ${user.uid}`);
-    }).catch((err) => {
-        console.error(`Error creating user ${user.uid}`, err);
+exports.setupFirestoreUser = functions
+    .region("europe-west1")
+    .auth.user()
+    .onCreate(async(user) => {
+        var firestoreUser = {
+            uid: user.uid,
+            email: user.email,
+        };
+        user.providerData.forEach((profile, index) => {
+            if (
+                index === 0 &&
+                profile.displayName != null &&
+                profile.displayName != undefined
+            ) {
+                firestoreUser.displayName = profile.displayName;
+            }
+            if (profile.providerId === "google.com") {
+                firestoreUser.googleProfile = {
+                    email: profile.email != null && profile.email != undefined ?
+                        profile.email :
+                        null,
+                    photoURL: profile.photoURL != null && profile.photoURL != undefined ?
+                        profile.photoURL :
+                        null,
+                    uid: profile.uid != null && profile.uid != undefined ?
+                        profile.uid :
+                        null,
+                };
+            }
+
+            if (profile.providerId === "apple.com") {
+                firestoreUser.appleProfile = {
+                    email: profile.email != null && profile.email != undefined ?
+                        profile.email :
+                        null,
+                    photoURL: profile.photoURL != null && profile.photoURL != undefined ?
+                        profile.photoURL :
+                        null,
+                    uid: profile.uid != null && profile.uid != undefined ?
+                        profile.uid :
+                        null,
+                };
+            }
+        });
+        await admin
+            .firestore()
+            .collection("users")
+            .doc(user.uid)
+            .set(firestoreUser)
+            .then(() => {
+                console.log(user.providerData[0]);
+                console.log(`Successfully created user ${user.uid}`);
+            })
+            .catch((err) => {
+                console.error(`Error creating user ${user.uid}`, err);
+            });
     });
-});
